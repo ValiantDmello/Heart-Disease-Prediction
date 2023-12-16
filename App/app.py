ca7c28784bbb65496214e28c37c41275b99a0da7
Valiant Dmello <vvd0927@gmail.com>
2023-12-16 14:23:57 -0500

    added requirements.txt

--- App/app.py
+++ App/app.py
@@ -1,8 +1,6 @@
 from flask import Flask, render_template, url_for, request, redirect
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
 
 app = Flask(__name__)
 
